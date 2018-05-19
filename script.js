5f184c2a148d2ae0c499635b9df04c866bcf226a
Brandon Herford <brandonherford@gmail.com>
2018-05-19 13:18:35 -0400

    add more pebbles and work on reset button

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-  $( function() {
-    $( "#draggable" ).draggable();
-  } );
+  
+$( function() {
+  $( 'img' ).draggable();
+} );
+
+var container = document.getElementById("container"); 
+
+// $( function(reset) {
+//   var container = document.getElementById("container");
+//   container.innerHTML= html;
+// } );
+
+$("#reset").click(function () {
+    $("img").animate({
+        top: "0px",
+        left: "0px"
+    });
+});
