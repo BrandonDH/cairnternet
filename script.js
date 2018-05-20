771442f8b9c0a634e519e0e414ea7e3d1fafd707
Brandon Herford <brandonherford@gmail.com>
2018-05-19 20:33:41 -0400

    Update email link and some js

--- script.js
+++ script.js
@@ -10,9 +10,21 @@ var container = document.getElementById("container");
 //   container.innerHTML= html;
 // } );
 
-$("#reset").click(function () {
-    $("img").animate({
-        top: "0px",
-        left: "0px"
+// $("#reset").click(function () {
+//     $("img").animate({
+//         top: "0px",
+//         left: "0px"
+//     });
+// });
+
+$('img').data({
+    'originalLeft': $('img').css('left'),
+    'origionalTop': $('img').css('top')
+});
+
+$(".reset").click(function() {
+    $('img').css({
+        'left': $('img').data('originalLeft'),
+        'top': $('img').data('img')
     });
 });
