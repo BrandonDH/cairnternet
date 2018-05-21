a0930568947d7b86e1ff9e3d34f3ea017b72a23e
Brandon Herford <brandonherford@gmail.com>
2018-05-21 07:12:36 -0400

    Add markup and JS modifications from prof Campbell

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
   
-$( function() {
-  $( 'img' ).draggable();
-} );
-
-var container = document.getElementById("container"); 
-
-// $( function(reset) {
-//   var container = document.getElementById("container");
-//   container.innerHTML= html;
+// $( function() {
+//   $( 'img' ).draggable();
 // } );
 
-// $("#reset").click(function () {
-//     $("img").animate({
-//         top: "0px",
-//         left: "0px"
+// var container = document.getElementById("container"); 
+
+// // $( function(reset) {
+// //   var container = document.getElementById("container");
+// //   container.innerHTML= html;
+// // } );
+
+// // $("#reset").click(function () {
+// //     $("img").animate({
+// //         top: "0px",
+// //         left: "0px"
+// //     });
+// // });
+
+// $('img').data({
+//     'originalLeft': $('img').css('left'),
+//     'origionalTop': $('img').css('top')
+// });
+
+// $(".reset").click(function() {
+//     $('img').css({
+//         'left': $('img').data('originalLeft'),
+//         'top': $('img').data('img')
 //     });
 // });
 
-$('img').data({
-    'originalLeft': $('img').css('left'),
-    'origionalTop': $('img').css('top')
-});
+var count = 1;
 
-$(".reset").click(function() {
-    $('img').css({
-        'left': $('img').data('originalLeft'),
-        'top': $('img').data('img')
+$( function() {
+    $( ".pebble" ).click(function () {                                
+        $(this).duplicate(document.getElementById('container').value).insertAfter("#the_cairn");
+        $(".draggable").draggable();
+        alert(document.getElementById('container').innerHTML);
     });
-});
+} );
+
+/* extend JQuery function (fn) member to create duplicates */ 
+$.fn.duplicate = function(cloneEvents) {
+    for ( var i = 0; i < count; i++ ) {
+        var tmp = [];
+        var the_clone = this.clone(cloneEvents);
+        the_clone.addClass('draggable');
+        $.merge(tmp, the_clone.get());
+        return this.pushStack(tmp);
+    }
+};
