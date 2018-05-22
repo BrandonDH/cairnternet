fdced63f5ff203e608cb6fb4852518dd596c7ed7
Brandon Herford <brandonherford@gmail.com>
2018-05-22 18:22:46 -0400

    Update JS, add new pebble, update styles and markup

--- script.js
+++ script.js
@@ -29,13 +29,41 @@
 //     });
 // });
 
+// var count = 1;
+
+// $( function() {
+//     $( ".pebble" ).click(function () {                                
+//         $(this).duplicate(document.getElementById('container').value).insertAfter("#the_cairn");
+//         $(".draggable").draggable();
+//         alert(document.getElementById('container').innerHTML);
+//     });
+// } );
+
+/* extend JQuery function (fn) member to create duplicates */ 
+// $.fn.duplicate = function(cloneEvents) {
+//     for ( var i = 0; i < count; i++ ) {
+//         var tmp = [];
+//         var the_clone = this.clone(cloneEvents);
+//         the_clone.addClass('draggable medium');
+//         $.merge(tmp, the_clone.get());
+//         return this.pushStack(tmp);
+//     }
+// };
+
+
+// Javascript contribution from Professor Bruce Campbell - RISD
 var count = 1;
+var zndx = 3;
+var rotation = 0;
 
 $( function() {
-    $( ".pebble" ).click(function () {                                
-        $(this).duplicate(document.getElementById('container').value).insertAfter("#the_cairn");
-        $(".draggable").draggable();
-        alert(document.getElementById('container').innerHTML);
+    $( ".pebble" ).click(function () {                              
+        $( this ).duplicate(document.getElementById('container').value).insertAfter("#the_cairn");
+        $( ".draggable" ).draggable();
+        $( ".draggable" ).click(function () {
+            rotation += 10;
+            $( this ).rotate(rotation);
+        });
     });
 } );
 
@@ -45,7 +73,16 @@ $.fn.duplicate = function(cloneEvents) {
         var tmp = [];
         var the_clone = this.clone(cloneEvents);
         the_clone.addClass('draggable');
+        the_clone.css("position", "absolute");
+        the_clone.css("left", "-500px");
+        the_clone.css("z-index", zndx++);
+        rotation = 0;
         $.merge(tmp, the_clone.get());
         return this.pushStack(tmp);
     }
+};
+
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
+    return $(this);
 };
