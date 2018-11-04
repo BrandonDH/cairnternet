a718669007be3dda6296a0100aad196556d2295e
Brandon Herford <brandonherford@gmail.com>
2018-11-03 22:16:23 -0400

    Replace click with drag

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $( function() {
     $( ".pebble" ).click(function () {                              
         $( this ).duplicate(document.getElementById('container').value).insertAfter("#the_cairn");
         $( ".draggable" ).draggabilly();
-        $( ".rotate" ).click(function () {
+        $( ".rotate" ).draggabilly(function () {
             rotation += 45;
             $( this ).rotate(rotation);
         });
